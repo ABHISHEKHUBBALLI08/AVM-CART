4baa1366bab0ffcbd512766c20d12d28e363dcc2
ABHISHEK HUBBALLI <159941697+ABHISHEKHUBBALLI08@users.noreply.github.com>
2024-10-13 13:40:36 +0530

    Create Testimonials.jsx

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import Slider from "react-slick";
+
+const TestimonialData = [
+  {
+    id: 1,
+    name: "Victor",
+    text: "Trendy Collection! AVM always has the latest styles. I’m impressed with the quality and the affordability of their products!",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 2,
+    name: "Satya ",
+    text: "Fast Shipping and Excellent Service! My order arrived earlier than expected, and the customer support was very helpful.",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 3,
+    name: "Anand",
+    text: "Great Value for Money! I love the variety and discounts offered. I got everything I needed at a fraction of the price!",
+    img: "https://picsum.photos/104/104",
+  },
+  {
+    id: 5,
+    name: "Sachin ",
+    text: "Amazing Quality! The products from AVM Shopping Cart exceeded my expectations. The fabric and design are just perfect!",
+    img: "https://picsum.photos/103/103",
+  },
+];
+
+const Testimonials = () => {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 10000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="py-10 mb-10">
+      <div className="container bg-primary/40">
+        {/* header section */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+          <p data-aos="fade-up" className="text-sm text-gray-950">
+            What our customers are saying
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Testimonials
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-900">
+            
+          </p>
+        </div>
+
+        {/* Testimonial cards */}
+        <div data-aos="zoom-in">
+          <Slider {...settings}>
+            {TestimonialData.map((data) => (
+              <div className="my-6">
+                <div
+                  key={data.id}
+                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative"
+                >
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-4">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-900">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                        {data.name}
+                      </h1>
+                    </div>
+                  </div>
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                    ,,
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
